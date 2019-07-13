aecb9899f5716da0d56297cc9287049d6a8e9fff
German <German@DESKTOP-JDLKDN3>
2019-07-12 21:58:39 -0600

    Agregada la conexion con la base de datos y creacion de la la clase conexion para realizar dicha conexion

---
+++ ProyectoEstacionamiento-develop/Proyecto_Negocios_IIP/ClaseConexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Negocios_IIP
+{
+    //EstacionamientoConnectionString
+    class ClaseConexion
+    {
+        public static string Cn = @"server = (local)\SQLEXPRESS; Initial Catalog = Estacionamiento; Integrated Security = True;";
+    }
+}
